fd8707218bcd5657eb56c0bc4faca1a12d74e013
Chris Aumann <me@chr4.org>
2014-08-06 18:10:23 +0200

    Add tests

---
+++ basicauth_test.go
@@ -0,0 +1,52 @@
+package basicauth
+
+import (
+	"net/http"
+	"testing"
+)
+
+// @todo rewrite tests
+func TestRetrieveCredentials(t *testing.T) {
+	var header map[string][]string
+
+	req, err := http.NewRequest("GET", "https://example.com/signup", nil)
+	if err != nil {
+		t.Error("getFromRequest(): Error creating HTTP request: %v", err)
+	}
+
+	// Test without HTTP Basic Auth header
+	if _, _, err := RetrieveCredentials(req); err == nil {
+		t.Error("getFromRequest(): Didn't fail when not specifying 'HTTP Basic Auth' header")
+	}
+
+	// Test with invalid Authorization header
+	header = map[string][]string{
+		"Authorization": {"INVALID AUTH_HEADER"},
+	}
+	req.Header = header
+	if _, _, err := RetrieveCredentials(req); err == nil {
+		t.Error("getFromRequest(): Didn't fail when using invalid 'Authorization' header")
+	}
+
+	// Test with invalid HTTP Basic Auth header
+	header = map[string][]string{
+		"Authorization": {"Basic INVALID"},
+	}
+	req.Header = header
+	if _, _, err := RetrieveCredentials(req); err == nil {
+		t.Error("getFromRequest(): Didn't fail when using invalid 'HTTP Basic Auth' header")
+	}
+
+	// Test valid HTTP Basic Auth header
+	req.SetBasicAuth("sarah", "i love icecream")
+	username, password, err := RetrieveCredentials(req)
+	if err != nil {
+		t.Error("getFromRequest(): Didn't succeed with valid HTTP Basic Auth header")
+	}
+	if string(username) != "sarah" {
+		t.Error("Username didn't match")
+	}
+	if string(password) != "i love icecream" {
+		t.Error("Password didn't match")
+	}
+}
